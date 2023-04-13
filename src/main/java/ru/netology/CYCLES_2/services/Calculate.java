8e41871bbeb37b387708ff25465f03ee59c39514
Таня Кишинская <lolo-20@yandex.ru>
2023-04-13 18:39:26 +0300

    fix

--- src/main/java/ru/netology/CYCLES_2/services/Calculate.java
+++ src/main/java/ru/netology/CYCLES_2/services/Calculate.java
@@ -1,14 +1,15 @@
 package ru.netology.CYCLES_2.services;
+
 public class Calculate {
     public int calculate(int income, int exp, int threshold) {
         int count = 0;
         int money = 0;
-//        int threshold = 20_000;
+
         for (int i = 1; i < 13; i++) {
             if (money >= threshold) {
                 System.out.println("месец " + i + " денег " + money + " -Буду отдыхать");
                 money = money - exp;
-                money = money - (8_000);
+                money = money / 3;//траты на отдых
                 count++;
             } else {
                 System.out.println("месец " + i + " Денег " + money + " -Работаем");
--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -9,15 +9,15 @@ public class CalculateTest {
         int count;
         Calculate calc = new Calculate();
         count = calc.calculate(10000, 3000, 20_000);
-            System.out.println("Мы отдыхали " + count + " раз");
-        }
+        System.out.println("Мы отдыхали " + count + " раз");
+    }
 
-        @Test
-        public  void testUser2() {
-            int count;
-            Calculate calc = new Calculate();
-            count = calc.calculate(100_000, 60_000, 150_000);
-            System.out.println("Мы отдыхали " + count + " раз");
-        }
+    @Test
+    public void testUser2() {
+        int count;
+        Calculate calc = new Calculate();
+        count = calc.calculate(100_000, 60_000, 150_000);
+        System.out.println("Мы отдыхали " + count + " раз");
+    }
 }
 
